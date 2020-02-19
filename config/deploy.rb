69d691ec9c6c7bd7296f9565a906df0edc29dfa1
BAI-Bonjwa <baibonjwa@gmail.com>
2020-02-19 09:26:18 +0800

    Removed database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,7 +64,7 @@ task :deploy do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
+    # invoke :'rails:db_migrate'
     invoke :'deploy:cleanup'
 
     on :launch do
