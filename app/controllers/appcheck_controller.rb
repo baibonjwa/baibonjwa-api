3287e64a8e4bfe6b1c5cf0945637d3d1bb83fd75
BAI-Bonjwa <baibonjwa@gmail.com>
2020-03-10 13:54:56 +0800

    Add fix bug for remote sha not working

--- app/controllers/appcheck_controller.rb
+++ app/controllers/appcheck_controller.rb
@@ -6,15 +6,15 @@ class AppcheckController < ApplicationController
     if Rails.env.development?
       result[:current_branch] = `git rev-parse --abbrev-ref HEAD`.chomp
       result[:current_sha] = `git rev-parse --verify HEAD`.chomp
-      result[:remote_sha] = `git rev-parse --verify origin/master`.chomp
+      result[:remote_sha] = `git ls-remote \`git remote get-url origin\` | head -1 | cut -f 1`.chomp
     else
       result[:current_branch] = `cd ../scm && git rev-parse --abbrev-ref HEAD`.chomp
       result[:current_sha] = `cd ../scm && git rev-parse --verify HEAD`.chomp
-      result[:remote_sha] = `cd ../scm && git rev-parse --verify origin/master`.chomp
-      deploy_version = `ls ../ -Art | sort -n | tail -n 2 | sed -n '2p'`.chomp
-      result[:deploy_version] = Integer(deploy_version, exception: false)
+      result[:remote_sha] = `cd ../scm && git ls-remote \`git remote get-url origin\` | head -1 | cut -f 1`.chomp
     end
     result[:latest] = result[:current_sha] === result[:remote_sha]
+    deploy_version = `ls ../ -Art | sort -n | tail -n 2 | sed -n '2p'`.chomp
+    result[:deploy_version] = Integer(deploy_version, exception: false)
 
     render json: result
   rescue => e
