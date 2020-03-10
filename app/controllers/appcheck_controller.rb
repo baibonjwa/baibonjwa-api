c02ecbafc3e7e46647ac4104b7d796be4f47de45
BAI-Bonjwa <baibonjwa@gmail.com>
2020-03-10 13:44:03 +0800

    Fix bug that no .git directory on production environment

--- app/controllers/appcheck_controller.rb
+++ app/controllers/appcheck_controller.rb
@@ -2,12 +2,20 @@ class AppcheckController < ApplicationController
   def appcheck
     result = {}
     result[:status] = 'ok'
-    result[:current_branch] = `git rev-parse --abbrev-ref HEAD`.chomp
-    result[:current_sha] = `git rev-parse --verify HEAD`.chomp
-    result[:remote_sha] = `git rev-parse --verify origin/master`.chomp
+
+    if Rails.env.development?
+      result[:current_branch] = `git rev-parse --abbrev-ref HEAD`.chomp
+      result[:current_sha] = `git rev-parse --verify HEAD`.chomp
+      result[:remote_sha] = `git rev-parse --verify origin/master`.chomp
+    else
+      result[:current_branch] = `cd ../scm && git rev-parse --abbrev-ref HEAD`.chomp
+      result[:current_sha] = `cd ../scm && git rev-parse --verify HEAD`.chomp
+      result[:remote_sha] = `cd ../scm && git rev-parse --verify origin/master`.chomp
+      deploy_version = `ls ../ -Art | sort -n | tail -n 2 | sed -n '2p'`.chomp
+      result[:deploy_version] = Integer(deploy_version, exception: false)
+    end
     result[:latest] = result[:current_sha] === result[:remote_sha]
-    deploy_version = `ls ../ -Art | sort -n | tail -n 2 | sed -n '2p'`.chomp
-    result[:deploy_version] = Integer(deploy_version, exception: false)
+
     render json: result
   rescue => e
     raise e unless Rails.env.development?
